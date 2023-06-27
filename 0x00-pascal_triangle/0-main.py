71cf7da3557ba86a675a532c5fd1d20817a39220
Mahmoud shee <mahmoudshee8914@gmail.com>
2023-06-27 14:25:10 -0700

    kenty

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -1,11 +1,8 @@
-import importlib.util
-
-# Dynamically import the pascal_triangle module
-module_name = "0-pascal_triangle"
-module_spec = importlib.util.spec_from_file_location(module_name, f"{module_name}.py")
-module = importlib.util.module_from_spec(module_spec)
-module_spec.loader.exec_module(module)
-pascal_triangle = module.pascal_triangle
+#!/usr/bin/python3
+"""
+0-main
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
 def print_triangle(triangle):
     """
@@ -14,7 +11,6 @@ def print_triangle(triangle):
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
-if __name__ == "__main__":
-    triangle = pascal_triangle(5)
-    print_triangle(triangle)
 
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
