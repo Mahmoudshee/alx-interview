f00112e5d2bdb60e3bd607fa43320d811da348cc
mahmoudshee <Mahmoudshee8914@gmail.com>
2023-08-03 00:40:05 -0700

    kenty

---
+++ 0x05-nqueen/0-nqueens.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+import sys
+
+def is_safe(board, row, col):
+    for prev_row in range(row):
+        if board[prev_row] == col or \
+           board[prev_row] - prev_row == col - row or \
+           board[prev_row] + prev_row == col + row:
+            return False
+    return True
+
+def solve_nqueens(N):
+    solutions = []
+    def backtrack(row, board):
+        if row == N:
+            solutions.append([[row, col] for row, col in enumerate(board)])
+            return
+        for col in range(N):
+            if is_safe(board, row, col):
+                board[row] = col
+                backtrack(row + 1, board)
+                board[row] = -1
+
+    if not isinstance(N, int):
+        print("N must be a number")
+        sys.exit(1)
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    
+    board = [-1] * N
+    backtrack(0, board)
+    
+    for solution in solutions:
+        print(solution)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    
+    solve_nqueens(N)
