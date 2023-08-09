07a15e54d6a84e21b42d7a5d3abd7c6c708eec73
mahmoudshee <Mahmoudshee8914@gmail.com>
2023-08-09 03:52:52 -0700

    kenty

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,36 @@
 #!/usr/bin/node
 
 const request = require('request');
-
 const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
-}
-
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
-    process.exit(1);
-  }
-  const film = JSON.parse(body);
-  const characters = film.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, function (error, response, body) {
-      if (error) {
-        console.error(error);
-        process.exit(1);
-      }
-      const character = JSON.parse(body);
-      console.log(character.name);
+  } else {
+    const characters = JSON.parse(body).characters;
+    const promises = characters.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, function (error, response, body) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
+      });
     });
-  });
+
+    Promise.all(promises)
+      .then(names => {
+        names.sort();
+        for (const name of names) {
+          console.log(name);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
 });
+
