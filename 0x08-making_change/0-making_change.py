b9707f2ade1fcecfc4b80c9d9afaa7f31f0b1c4b
Mahmoudshee <mahmoudshee8914@gmail.com>
2023-08-23 03:23:30 -0700

    kenty

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.4
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for value in range(coin, total + 1):
+            dp[value] = min(dp[value], dp[value - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+my readme file for  Change comes from within
