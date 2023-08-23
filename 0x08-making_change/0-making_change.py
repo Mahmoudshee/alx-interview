a4dc6ff720d4c7b1b8503d94d2163d8ed0312c61
Mahmoudshee <mahmoudshee8914@gmail.com>
2023-08-23 07:41:11 -0700

    kenty

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,22 +12,28 @@ def makeChange(coins, total):
         int: Fewest number of coins needed to meet the total.
             Returns -1 if total cannot be met.
     """
+    # Check if the total amount is 0 or less
     if total <= 0:
         return 0
 
+    # Initialize a list to store the minimum number of coins needed for each total value
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
+    # Iterate through each coin value in the list
     for coin in coins:
+        # Iterate through each possible total value
         for i in range(coin, total + 1):
+            # Calculate the minimum number of coins needed for the current total value
             dp[i] = min(dp[i], dp[i - coin] + 1)
 
+    # If the minimum number of coins for the total amount is still infinity, it's not possible to make change
     if dp[total] == float('inf'):
         return -1
 
+    # Return the minimum number of coins needed to meet the total amount
     return dp[total]
 
 # Test cases
-print(makeChange([1, 2, 25], 37))
-print(makeChange([1256, 54, 48, 16, 102], 1453))
-
+print(makeChange([1, 2, 25], 37))  # Expected output: 7
+print(makeChange([1256, 54, 48, 16, 102], 1453))  # Expected output: -1
